63e98dee62e698c6d8fd67e57e708f7cf329320d
hakan celebi <hcelebi@ebay.com>
2021-04-09 15:43:14 +0300

    add message response to message controller

--- src/main/java/com/gg/phnx/learningjourneyprovider/controller/MessageController.java
+++ src/main/java/com/gg/phnx/learningjourneyprovider/controller/MessageController.java
@@ -1,4 +1,19 @@
 package com.gg.phnx.learningjourneyprovider.controller;
 
+import com.gg.phnx.learningjourneyprovider.model.MessageResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
 public class MessageController {
+
+    @GetMapping
+    public MessageResponse getMessage() {
+        MessageResponse messageResponse = new MessageResponse();
+        messageResponse.setMessage("Hello World Message From Provider");
+        return messageResponse;
+    }
 }
