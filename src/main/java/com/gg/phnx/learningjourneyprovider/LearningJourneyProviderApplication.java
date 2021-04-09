86e6174eafa5dec55e0844ccd0872c5fab18c59c
ahmet yilmaz <ayilmaz@ebay.com>
2021-04-09 15:19:51 +0300

    Create projects

---
+++ src/main/java/com/gg/phnx/learningjourneyprovider/LearningJourneyProviderApplication.java
@@ -0,0 +1,13 @@
+package com.gg.phnx.learningjourneyprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJourneyProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningJourneyProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gg/phnx/learningjourneyprovider/LearningJourneyProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.phnx.learningjourneyprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJourneyProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
