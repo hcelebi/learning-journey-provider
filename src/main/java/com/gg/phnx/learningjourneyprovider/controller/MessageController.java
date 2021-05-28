dadbc4f1076521db0850683e7861710735263990
hakan celebi <hcelebi@ebay.com>
2021-05-28 14:48:47 +0300

    delete unused imports

--- src/main/java/com/gg/phnx/learningjourneyprovider/controller/MessageController.java
+++ src/main/java/com/gg/phnx/learningjourneyprovider/controller/MessageController.java
@@ -1,11 +1,8 @@
 package com.gg.phnx.learningjourneyprovider.controller;
 
 import com.gg.phnx.learningjourneyprovider.model.MessageResponse;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
