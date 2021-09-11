1af7b6b657378f2ca744f56c1ee7b8a99e3e1230
hakan celebi <hcelebi@ebay.com>
2021-09-11 23:37:19 +0300

    fix form field type problem

--- src/test/java/com/gg/phnx/learningjourneyprovider/controller/MessageControllerProviderTest.java
+++ src/test/java/com/gg/phnx/learningjourneyprovider/controller/MessageControllerProviderTest.java
@@ -5,6 +5,7 @@ import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvide
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,12 +14,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Provider("learning-journey-provider")
+@Provider("helloworld-demo-provider")
 @PactFolder("pacts")
 @PactBroker(
-    host = "10.35.101.51",
-    port = "30266",
-    scheme = "http",
+    host = "hakobe.pactflow.io",
+    authentication = @PactBrokerAuth(token = "bZCMbZnsXOhPJ0x4H6wVUg"),
+    scheme = "https",
     enablePendingPacts = "true",
     includeWipPactsSince = "2021-08-01",
     providerTags = "main"
